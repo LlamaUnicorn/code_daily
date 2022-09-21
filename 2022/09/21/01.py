47636750a0c22150c935b693d952ecc80de9827d
LlamaUnicorn <55321531+LlamaUnicorn@users.noreply.github.com>
2022-09-21 12:25:46 +0000

    #replit100daysofcode Day 1

---
+++ 2022/09/21/01.py
@@ -0,0 +1,39 @@
+#replit100DaysOfCode
+#day 1
+
+
+# print("Hello Replit")
+
+# print("Well we")
+# print("just use more lines")
+# print("of code")
+
+# print("""Anything that starts
+# with three quotes, and ends
+# in three quotes can span
+# many lines and even contain " symbols
+# within it without freaking anything out!""")
+
+# Print("What could go wrong?")
+
+# print "Hello Again"
+
+# print(Please work)
+
+# print("@= MUSIC+ =")
+# print("> Songs")
+# print("> Albums")
+# print("> Artists")
+
+name = "Dave Carell"
+date = "21/09/2022"
+message = """
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+"""
+print(name)
+print(date)
+print(message)
+print("I am feeling :)")
+print("You can follow my progress at replit.com/@")
